579ea0ee4600442c3c76194f322b1d372907b4ea
Andreas Grønlien <andreas@gronlien.no>
2021-08-12 15:00:01 +0200

    Moved variables into separate files

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("service-account-key.json")
+  credentials = file(var.credentials_file)
 
-  project = "idyllic-cabinet-322408"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
